758fbf4481c83a78e7d35a2634b2e44d8c5ca283
Alejandro Alvarez <eliluminado00@gmail.com>
2017-09-03 18:38:21 -0300

    Support for multiple connections in example

--- examples/index.js
+++ examples/index.js
@@ -3,36 +3,74 @@ const hapiRouteLogging = require('../lib/index');
 
 const server = new Hapi.Server();
 
-server.connection({
-  port: 3000, host: 'localhost'
+const web = server.connection({
+  host: 'localhost',
+  port: 3000,
+  labels: ['web']
 });
 
-server.register([{
-  register: hapiRouteLogging
+const admin = server.connection({
+  host: 'localhost',
+  port: 3030,
+  labels: ['admin']
+});
+
+web.register([{
+  register: hapiRouteLogging,
+  options: {
+    showRoutes: true,
+    showRoutesList: true,
+    checkUpgrade: true
+  }
 }], err => {
   if (err) {
     console.error(err);
     throw err;
   }
 
-  server.route({
+  web.route({
     method: 'GET',
     path: '/route1',
     handler(request, reply) {
       reply('Testing route1');
     }
   });
-  server.route({
+  web.route({
     method: 'GET',
     path: '/route2',
     handler(request, reply) {
       reply('Testing route2');
     }
   });
+});
 
-  server.start(err => {
-    if (err) {
-      throw err;
+admin.register([{
+  register: hapiRouteLogging,
+  options: {
+    showRoutes: true,
+    showRoutesList: true,
+    checkUpgrade: false
+  }
+}], err => {
+  if (err) {
+    console.error(err);
+    throw err;
+  }
+
+  admin.route({
+    method: 'GET',
+    path: '/book/{id}',
+    config: {
+      description: 'Get book by ID'
+    },
+    handler(request, reply) {
+      reply('Get book');
     }
   });
+});
+
+server.start(err => {
+  if (err) {
+    throw err;
+  }
 });
